a0c90de9e058c51ff3dfe51be1230d6cbc6271cf
David Peden <davidpeden3@gmail.com>
2014-10-09 14:18:05 -0500

    definitely have to keep lifecycle like this because octopus perfors transforms AFTER predeploy. also trying to fix path issue for cspack.

---
+++ source/Deployment/postdeploy.ps1
@@ -0,0 +1,8 @@
+﻿Write-Host "Repacking Azure Web Package"
+
+$role = "OctopusVariableSubstitutionTester"
+$rolePath = "azurepackage/webrole/approot"
+$webPath = "azurepackage/webrole/sitesroot/0"
+$cspackPath = "C:\Program Files\Microsoft SDKs\Windows Azure\.NET SDK\v2.3\bin\cspack.exe"
+
+& $cspackPath "ServiceDefinition.csdef" "/out:Azure.ccproj.cspkg" "/role:$role;$rolePathOctopusVariableSubstitutionTester.dll" "/rolePropertiesFile:$role;cspackproperties.txt" "/sites:$role;Web;$webPath" "/sitePhysicalDirectories:$role;Web;$webPath"
