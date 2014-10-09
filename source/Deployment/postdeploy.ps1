4155a926a3e5319b42c3797b50cbb83f18ac69ef
David Peden <davidpeden3@gmail.com>
2014-10-09 14:21:53 -0500

    add missing ;

--- source/Deployment/postdeploy.ps1
+++ source/Deployment/postdeploy.ps1
@@ -5,4 +5,4 @@ $rolePath = "azurepackage/webrole/approot"
 $webPath = "azurepackage/webrole/sitesroot/0"
 $cspackPath = "C:\Program Files\Microsoft SDKs\Windows Azure\.NET SDK\v2.3\bin\cspack.exe"
 
-& $cspackPath "ServiceDefinition.csdef" "/out:Azure.ccproj.cspkg" "/role:$role;$rolePathOctopusVariableSubstitutionTester.dll" "/rolePropertiesFile:$role;cspackproperties.txt" "/sites:$role;Web;$webPath" "/sitePhysicalDirectories:$role;Web;$webPath"
+& $cspackPath "ServiceDefinition.csdef" "/out:Azure.ccproj.cspkg" "/role:$role;$rolePath;OctopusVariableSubstitutionTester.dll" "/rolePropertiesFile:$role;cspackproperties.txt" "/sites:$role;Web;$webPath" "/sitePhysicalDirectories:$role;Web;$webPath"
